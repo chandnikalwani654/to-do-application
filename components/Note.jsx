634d3bd34c5e6cc7de0d7aa35ac9e37c8b8bdfdb
githubchandni <113281125+githubchandni@users.noreply.github.com>
2024-06-12 19:23:24 +0530

    Add files via upload

---
+++ components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Note(){
+    return(
+        <div className="note">
+            <h1>This is the heading of the note</h1>
+            <p>this is the paragraph of the note</p>
+        </div>
+    )
+}
+export default Note;
